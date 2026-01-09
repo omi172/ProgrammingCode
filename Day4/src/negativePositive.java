afa41cc96c1ea21b512ad2b2763ebc4057ea4105
amit-at-723322665394 <amitroy4556@gmail.com>
2026-01-09 19:49:34 +0530

    adding negative and positive files

---
+++ Day4/src/negativePositive.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class negativePositive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if(number < 0){
+            System.out.println("Number is negative");
+        }else{
+            System.out.println("Number is positive");
+        }
+    }
+}
