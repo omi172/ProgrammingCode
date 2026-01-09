a68d94f095abfb678c41c2a5dceb756718d35106
amit-at-723322665394 <amitroy4556@gmail.com>
2026-01-09 17:16:26 +0530

    adding pattern1 file

---
+++ Day4/src/pattern1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = n;
+        for(int i = 0; i < n; i++){
+            a = n;
+            for(int j = 0; j < n; j++){
+                if(i > j){
+                    System.out.print(a + " ");
+                    a--;
+                }else{
+                    System.out.print(a + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
