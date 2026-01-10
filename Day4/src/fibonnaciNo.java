6dcf02c157e14a4bc2e1766aa7414ee219f28fbf
Amit Roy <48159756+omi172@users.noreply.github.com>
2026-01-10 09:23:34 +0530

    Update fibonnaciNo.java

--- Day4/src/fibonnaciNo.java
+++ Day4/src/fibonnaciNo.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class fibonnaciNo {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+         int n1 = 0, n2 = 1;
+        for (int i = 0; i < N; i++) {
+            System.out.print(n1 + " ");
+            int n3 = n2 + n1;
+            n1 = n2;
+            n2 = n3;
+        }
     }
 }
