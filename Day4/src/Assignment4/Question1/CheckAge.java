a74f873f31fb5436e1881bdc0245b796f17a130b
Amit Roy <48159756+omi172@users.noreply.github.com>
2026-02-03 21:35:38 +0530

    Update CheckAge.java

--- Day4/src/Assignment4/Question1/CheckAge.java
+++ Day4/src/Assignment4/Question1/CheckAge.java
@@ -6,8 +6,11 @@ public class CheckAge {
          this.studentsAge = student;
      }
     public void check() throws AgeNotWithinRangeException{
-            if(studentsAge < 15 || studentsAge > 21) {
+            if(studentsAge > 15 && studentsAge < 21) {
+                System.out.println("Age Is Valid");
+            }else{
                 throw new AgeNotWithinRangeException("Age is not within range");
             }
     }
 }
+
