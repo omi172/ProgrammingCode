054665d797cfe38418f85d0c58c872dbc5ce9390
Amit Roy <48159756+omi172@users.noreply.github.com>
2026-02-03 21:08:55 +0530

    Add files via upload

---
+++ Day4/src/Assignment4/Question5/Stack.java
@@ -0,0 +1,32 @@
+package Assignment4.Question5;
+
+import java.util.ArrayList;
+
+public class Stack {
+    public static void main(String[] args) {
+        ArrayList<Integer> store = new ArrayList<>();
+        pushElement(1, store);
+        pushElement(2, store);
+        pushElement(3, store);
+        pushElement(4, store);
+        pushElement(5, store);
+        popElement(store);
+        popElement(store);
+    }
+    static void pushElement(int element,ArrayList<Integer> store){
+        store.add(element);
+        System.out.println(element + " is Added");
+    }
+
+    static void popElement(ArrayList<Integer> store){
+        System.out.println(store.get(store.size() - 1) + " Is Removed");
+        store.remove(store.size() -1);
+    }
+    static void checkIsEmpty(ArrayList<Integer> store){
+        if(store.size() == 0){
+            System.out.println("Stack Is Empty");
+        }else{
+            System.out.println("Stack Is Not Empty");
+        }
+    }
+}
